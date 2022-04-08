c180e271a32168532b0eb82b239c52f4228e1931
Chris Toph <crchristopherrivera@gmail.com>
2022-04-08 13:13:59 -0400

    added tailwindcss and swtiched css to it

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,67 +2,22 @@
 	import Spinner from '../components/tripleSpinner.svelte';
 </script>
 
-<nav id="navbar">
-	<div class="flex-middle" id="toph">
-		<a class="nav-link font-baloo2" href="/"> Toph </a>
+<nav class="flex fixed h-16 inset-x-0 z-50" id="navbar">
+	<div class="flex items-center px-4 w-full bg-white" id="toph">
+		<a class="font-baloo font-bold text-[2rem] no-underline text-black" href="/"> Toph </a>
 	</div>
-	<div class="flex-middle" id="projects">
-		<a class="nav-link font-baloo2" href="/"> Projects </a>
+	<div class="flex items-center px-4 bg-gray-300" id="projects">
+		<a class="font-baloo font-bold text-[2rem] no-underline text-black" href="/"> Projects </a>
 	</div>
-	<div class="flex-middle" id="experience">
-		<a class="nav-link font-baloo2" href="/"> Experience </a>
+	<div class="flex items-center px-4 bg-gray-600" id="experience">
+		<a class="font-baloo font-bold text-[2rem] no-underline text-black" href="/"> Experience </a>
 	</div>
 </nav>
 
-<div id="bg-spinner">
+<div class="absolute -bottom-36 -right-36 z-0" id="bg-spinner">
 	<Spinner color="#000" colorSecondary="#000" spinnerSize="350px" />
 </div>
 
 <style>
-	#navbar {
-		display: flex;
-		position: fixed;
-		height: 4rem;
-
-		/* INFO: these two basically give it its width */
-		left: 0;
-		right: 0;
-		/* ^^ */
-
-		z-index: 100;
-	}
-
-	#navbar div {
-		padding-left: 1rem;
-		padding-right: 1rem;
-	}
-
-	#toph {
-		background-color: hsl(0, 0%, 100%);
-		width: 100%;
-	}
-
-	#projects {
-		background-color: hsl(0, 0%, 80%);
-	}
-
-	#experience {
-		background-color: hsl(0, 0%, 60%);
-	}
-
-	.nav-link,
-	.nav-link:any-link {
-		font-weight: 700;
-		font-size: 2rem;
-		line-height: 2rem;
-		text-decoration: none;
-		color: black;
-	}
-
-	#bg-spinner {
-		position: absolute;
-		bottom: -9rem;
-		right: -9rem;
-		z-index: 0;
-	}
+	/* changed to tailwindcss */
 </style>
